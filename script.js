f84221b9c0dbe15a504d7c6778d55e02c044167a
seejn <seejmaharjan@gmail.com>
2020-09-19 10:22:48 +0545

    side to side

--- script.js
+++ script.js
@@ -2,15 +2,23 @@ const close = () =>
 {
     let closebutton = document.querySelector('.close');
     let modal = document.querySelector('.modal');
-    let button = document.querySelector('button');
+    let button = document.querySelectorAll('button');
     let audio = document.querySelector('audio');
     closebutton.addEventListener("click", () =>
     {
+        console.log('closebutton');
         modal.classList.toggle('modalActive');
         audio.play();
     });
-    button.addEventListener("click", () =>
+    button[0].addEventListener("click", () =>
     {
+        console.log('closebutton');
+        modal.classList.toggle('modalActive');
+        audio.play();
+    });
+    button[1].addEventListener("click", () =>
+    {
+        console.log('closebutton');
         modal.classList.toggle('modalActive');
         audio.play();
     });
